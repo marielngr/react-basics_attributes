2ac094ecfce7ce113ef333d0cae79a8184dec4bd
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-13 11:26:15 +0100

    solve

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <ArticleComponent />;
+}
+
+function ArticleComponent() {
+  return (
+    <article className="article__title">
+      <h2 className="article__title">Titel</h2>
+      <label htmlFor="input">Hier kommt was rein!</label>
+      <input id="input" />
+      <a className="article__link" href="https://swapi.dev/">
+        Supriiiiiiise!
+      </a>
+    </article>
+  );
 }
