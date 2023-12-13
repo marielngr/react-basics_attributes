99d807168d7aeaad23a70477ad66b4292d4dfbe4
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-13 12:21:16 +0100

    fix nix

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ export default function App() {
 
 function ArticleComponent() {
   return (
-    <article className="article__title">
+    <article className="article">
       <h2 className="article__title">Titel</h2>
       <label htmlFor="input">Hier kommt was rein!</label>
       <input id="input" />
       <a className="article__link" href="https://swapi.dev/">
         Supriiiiiiise!
       </a>
+      <img src="https://picsum.photos/200"></img>
     </article>
   );
 }
